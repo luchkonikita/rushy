9b0caf93e6b9b0efd4173ec59c755859fe318f99
Nikita Luchko <nikita.luchko@toptal.com>
2018-06-08 14:31:06 +0400

    Fix lint offences

--- src/lighthouse.ts
+++ src/lighthouse.ts
@@ -1,7 +1,8 @@
+// tslint:disable-next-line no-var-requires
 const lighthouse = require('lighthouse')
 
 function getLighthouseReport(url: string, opts: any, config: any): any {
-	return lighthouse(url, opts, config)
+  return lighthouse(url, opts, config)
 }
 
 export { getLighthouseReport }
--- test/index.ts
+++ test/index.ts
@@ -34,8 +34,10 @@ test.beforeEach(t => {
     }
   }
 
-  t.context.chromeLaunchStub = sinon.stub(chromeLauncher, 'launch').returns(Promise.resolve(t.context.chromeStub))
-  t.context.lighthouseLaunchStub = sinon.stub(lighthouse, 'getLighthouseReport').returns(Promise.resolve(t.context.lighthouseResultsStub))
+  t.context.chromeLaunchStub = sinon.stub(chromeLauncher, 'launch')
+    .returns(Promise.resolve(t.context.chromeStub))
+  t.context.lighthouseLaunchStub = sinon.stub(lighthouse, 'getLighthouseReport')
+    .returns(Promise.resolve(t.context.lighthouseResultsStub))
 })
 
 test.afterEach(t => {
@@ -48,9 +50,11 @@ test.serial('generate report', async t => {
   const csv = fs.readFileSync('test/tmp/report_all.csv').toString()
 
   const expectedCsv = [
+    // tslint:disable max-line-length
     'Page,Time to first byte,First meaningful paint,First interactive,Consistently interactive,Total byte weight,Speed index metric',
     'https://example.com,time-to-first-byte value,first-meaningful-paint value,first-interactive value,consistently-interactive value,total-byte-weight value,speed-index-metric value',
     ''
+    // tslint:enable max-line-length
   ].join('\n')
 
   t.true(t.context.chromeStub.kill.calledOnce)
