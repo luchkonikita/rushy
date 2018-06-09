0276996c6e0c211b6acae672a80ab2b0987d7e8c
Nikita Luchko <nikita.luchko@toptal.com>
2018-06-09 10:24:45 +0400

    Introduce concurrency option
    You can spawn multiple chrome processes
    and run audits in parallel.
    
    Concurrency is done in a simple batching manner,
    can be improved in the future.
    
    Some local measurements:
    Running with 20 URLs.
    With concurrency of 4 - 216.14s
    With concurrency of 1 - 687.66s

--- src/runner.ts
+++ src/runner.ts
@@ -1,21 +1,32 @@
+import * as chromeLauncher from 'chrome-launcher'
 import { getLighthouseReport } from './lighthouse'
+import { isNumber } from 'lodash'
 import Config from './config'
 
 export default class Runner {
   private config: Config
+  private chrome: chromeLauncher.LaunchedChrome
 
   constructor(config: Config) {
     this.config = config
   }
 
+  async start() {
+    this.chrome = await chromeLauncher.launch({ ...this.config.chromeOpts })
+  }
+
+  async stop() {
+    await this.chrome.kill()
+  }
+
   async runAudit(url): Promise<IAuditReport> {
-    const results = await getLighthouseReport(url, { port: this.config.chromePort }, this.config.lighthouseOpts)
+    const results = await getLighthouseReport(url, { port: this.chrome.port }, this.config.lighthouseOpts)
     delete results.artifacts
 
     const urlReport: IAuditReport = {}
     this.config.auditKeys.forEach(key => {
       const data = results.audits[key] as IAudit
-      urlReport[key] = data.rawValue
+      urlReport[key] = isNumber(data.rawValue) ? Math.round(data.rawValue) : data.rawValue
     })
     return urlReport
   }
